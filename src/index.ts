2cec3287b67d5f2588db31fc19b2e0a1ca3e2f1d
BenjaminKuznetsov <benjaminkuznetsoff@gmail.com>
2024-10-09 21:05:56 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express"
+const app = express()
+const port = 3003
+
+app.get("/", (req: Request, res: Response) => {
+  let helloPhrase = "Hello World!+++ ******"
+
+  res.send(helloPhrase)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
